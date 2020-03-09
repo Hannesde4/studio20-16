23e708621b5dcfc321fe899dc8712ec154e8505d
Hannes De Baere <hannes_de_baere@hotmail.com>
2020-03-09 13:46:45 +0100

    fixed some styling bugs, and reorganised the scripts

---
+++ app/js/scripts-homepage.js
@@ -0,0 +1,20 @@
+/* Script to toggle classes on home container for list/block view */
+
+let list = document.getElementById('list-view');
+let block = document.getElementById('block-view');
+let homepageContainer = document.getElementById('container-homepage');
+
+let toggleListView = () => {
+    //console.log("list is clicked");
+    homepageContainer.classList.add("list");
+    homepageContainer.classList.remove("block");
+}
+
+let toggleBlockView = () => {
+    //console.log("block is clicked");
+    homepageContainer.classList.remove("list");
+    homepageContainer.classList.add("block");
+}
+
+list.addEventListener("click", toggleListView);
+block.addEventListener("click", toggleBlockView);
